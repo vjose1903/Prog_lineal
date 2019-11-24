283604343210ee376a81ae675943506c5b898087
victor <vjose1903@outlook.es>
2019-11-24 12:09:14 -0400

    balanceo de oferta y demanda listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,48 @@ import { AppConfig } from '../environments/environment';
 })
 export class AppComponent implements OnInit {
   started = false;
-  sinRayas = true;
-  uno = false;
-  dos = false;
-  tres = false;
-  A = false;
-  B = false;
-  C = false;
-  D = false;
+  balanceado = false;
+
   imagenCapturaDatos = '../assets/img/sin.png';
   btnSelected: any;
 
   element: HTMLElement;
+
+  oferta_1 = 1000;
+  oferta_2 = 1000;
+  oferta_3 = 1000;
+  oferta_4;
+
+  demanda_A = 500;
+  demanda_B = 1000;
+  demanda_C = 1000;
+  demanda_D = 500;
+  demanda_E;
+
+  _1A = 1;
+  _1B = 1;
+  _1C = 1;
+  _1D = 1;
+
+  _2A = 1;
+  _2B = 1;
+  _2C = 1;
+  _2D = 1;
+
+  _3A = 1;
+  _3B = 1;
+  _3C = 1;
+  _3D = 1;
+
+  _4A;
+  _4B;
+  _4C;
+  _4D;
+
+  sumaOferta;
+  sumaDemanda;
+  balanceOferta = 0;
+  balanceDemanda = 0;
 
   constructor(public electronService: ElectronService, private translate: TranslateService) {}
 
@@ -54,6 +84,16 @@ export class AppComponent implements OnInit {
     }
     this.btnSelected = key;
   }
+  enfocarOferta(key) {
+    if (key === 'uno') {
+      this.imagenCapturaDatos = '../assets/img/uno.png';
+    } else if (key === 'dos') {
+      this.imagenCapturaDatos = '../assets/img/dos.png';
+    } else if (key === 'tres') {
+      this.imagenCapturaDatos = '../assets/img/tres.png';
+    }
+    this.btnSelected = key;
+  }
 
   enfocarInput(arg) {
     this.element = document.getElementById(arg) as HTMLElement;
@@ -62,5 +102,56 @@ export class AppComponent implements OnInit {
   desenfocar() {
     this.btnSelected = null;
     this.imagenCapturaDatos = '../assets/img/sin.png';
+  }
+
+  balancear() {
+    const sumaOferta = this.oferta_1 + this.oferta_2 + this.oferta_3;
+    const sumaDemanda = this.demanda_A + this.demanda_B + this.demanda_C + this.demanda_D;
+
+    this.sumaDemanda = sumaDemanda;
+    this.sumaOferta = sumaOferta;
+
+    if (sumaOferta === sumaDemanda) {
+      this.balanceDemanda = 0;
+      this.balanceOferta = 0;
+      //
+    } else if (sumaOferta < sumaDemanda) {
+      this.balanceOferta = sumaDemanda - sumaOferta;
+      this.sumaOferta = sumaDemanda;
+      this.balanceDemanda = 0;
+      //
+    } else if (sumaOferta > sumaDemanda) {
+      this.balanceDemanda = sumaOferta - sumaDemanda;
+      this.sumaDemanda = sumaOferta;
+      this.balanceOferta = 0;
+    }
+    this.balanceado = true;
+  }
+  habilitarBtnBanlancear() {
+    let res = true;
+    if (
+      this.oferta_1 !== undefined &&
+      this.oferta_2 !== undefined &&
+      this.oferta_3 !== undefined &&
+      this.demanda_A !== undefined &&
+      this.demanda_B !== undefined &&
+      this.demanda_C !== undefined &&
+      this.demanda_D !== undefined &&
+      this._1A !== undefined &&
+      this._1B !== undefined &&
+      this._1C !== undefined &&
+      this._1D !== undefined &&
+      this._2A !== undefined &&
+      this._2B !== undefined &&
+      this._2C !== undefined &&
+      this._2D !== undefined &&
+      this._3A !== undefined &&
+      this._3B !== undefined &&
+      this._3C !== undefined &&
+      this._3D !== undefined
+    ) {
+      res = false;
+    }
+    return res;
   }
 }
